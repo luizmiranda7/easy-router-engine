5d7273abfd0dda431303fc4e6c839b823a829c7d
Luiz Miranda <luiz.miranda@touchtec.com.br>
2016-01-21 13:33:23 -0200

    Fixing REST API Adding comments to REST API
    Change-Id: I6650641af90f61ae2a20ec11ce9c6174199ba930

--- src/main/java/br/com/easyrouter/engine/rest/RestController.java
+++ src/main/java/br/com/easyrouter/engine/rest/RestController.java
@@ -1,5 +1,7 @@
+
 package br.com.easyrouter.engine.rest;
- 
+
+
 import java.io.Writer;
 
 import javax.ws.rs.Consumes;
@@ -9,39 +11,49 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
-import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
-import com.thoughtworks.xstream.io.json.JsonWriter;
-
 import br.com.easyrouter.engine.api.RouteRequest;
 import br.com.easyrouter.engine.api.RouteResponse;
 import br.com.easyrouter.engine.solver.Solver;
- 
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.json.AbstractJsonWriter;
+import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
+import com.thoughtworks.xstream.io.json.JsonWriter;
+
+
 @Path("/hello")
 public class RestController {
-	
-	private static final XStream xstream = new XStream(new JsonHierarchicalStreamDriver() {
-	    public HierarchicalStreamWriter createWriter(Writer writer) {
-	        return new JsonWriter(writer, JsonWriter.DROP_ROOT_MODE);
-	    }
-	});
-	
-	static {
-	    xstream.setMode(XStream.NO_REFERENCES);
-	}
-	
-	@POST
-	@Path("/solve")
-	@Consumes(MediaType.TEXT_PLAIN)
-	@Produces(MediaType.TEXT_PLAIN)
-	public Response getMsg(String routeRequestAsString) {
-		RouteRequest routeRequest = (RouteRequest) xstream.fromXML(routeRequestAsString);
-		
-		RouteResponse routeResponse = Solver.solve(routeRequest);
-		
-		
-		return Response.status(200).entity(routeResponse).build();
-	}
- 
+
+    private static final XStream xstream = new XStream(new JsonHierarchicalStreamDriver() {
+
+        @Override
+        public HierarchicalStreamWriter createWriter(Writer writer) {
+            return new JsonWriter(writer, AbstractJsonWriter.DROP_ROOT_MODE);
+        }
+    });
+
+    static {
+        xstream.setMode(XStream.NO_REFERENCES);
+    }
+
+    /**
+     * HTTP method that given a {@link RouteRequest} returns the {@link RouteResponse} calculated
+     *
+     * @param routeRequestAsString
+     * @return {@link RouteResponse} calculated
+     */
+    @POST
+    @Path("/solve")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getMsg(String routeRequestAsString) {
+
+        RouteRequest routeRequest = (RouteRequest) xstream.fromXML(routeRequestAsString);
+        Solver solver = new Solver(routeRequest);
+        RouteResponse routeResponse = solver.solve();
+
+        return Response.status(200).entity(routeResponse).build();
+    }
+
 }
--- src/main/java/br/com/easyrouter/engine/solver/Solver.java
+++ src/main/java/br/com/easyrouter/engine/solver/Solver.java
@@ -1,16 +1,10 @@
+
 package br.com.easyrouter.engine.solver;
 
-import java.util.ArrayList;
+
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-import br.com.easyrouter.engine.api.DeliveryPoint;
-import br.com.easyrouter.engine.api.DistributionCenter;
-import br.com.easyrouter.engine.api.Driver;
-import br.com.easyrouter.engine.api.Order;
-import br.com.easyrouter.engine.api.RouteRequest;
-import br.com.easyrouter.engine.api.Vehicle;
 import jsprit.core.problem.Location;
 import jsprit.core.problem.driver.DriverImpl;
 import jsprit.core.problem.job.Service;
@@ -18,78 +12,84 @@ import jsprit.core.problem.solution.route.activity.TimeWindow;
 import jsprit.core.problem.vehicle.VehicleImpl;
 import jsprit.core.problem.vehicle.VehicleType;
 import jsprit.core.problem.vehicle.VehicleTypeImpl;
+import br.com.easyrouter.engine.api.DeliveryPoint;
+import br.com.easyrouter.engine.api.DistributionCenter;
+import br.com.easyrouter.engine.api.Driver;
+import br.com.easyrouter.engine.api.Order;
+import br.com.easyrouter.engine.api.RouteRequest;
+import br.com.easyrouter.engine.api.RouteResponse;
+import br.com.easyrouter.engine.api.Vehicle;
+
 
 public class Solver {
 
-	private final int WEIGHT_INDEX = 0;
-	private final int VOLUME_INDEX = 1;
-	
-	private Set<VehicleImpl> vehicleImpls = new HashSet<VehicleImpl>();
-	private Set<Service> services = new HashSet<Service>();
-	private Set<DriverImpl> drivers = new HashSet<DriverImpl>();
+    private final int WEIGHT_INDEX = 0;
+    private final int VOLUME_INDEX = 1;
 
-	/**
-	 * Constructs a capacitated vehicle routing problem with time windows.
-	 */
-	private Solver(RouteRequest routeRequest) {
-		DistributionCenter distributionCenter = routeRequest.getOrders().iterator().next().getDistributionCenter();
-		this.loadVehicles(routeRequest.getVehicles(), distributionCenter);
-		this.loadDrivers(routeRequest.getDrivers(), distributionCenter);
-		this.loadOrders(routeRequest.getOrders());
-	}
+    private Set<VehicleImpl> vehicleImpls = new HashSet<VehicleImpl>();
+    private Set<Service> services = new HashSet<Service>();
+    private Set<DriverImpl> drivers = new HashSet<DriverImpl>();
 
-	private void loadVehicles(Set<Vehicle> vehicles, DistributionCenter distributionCenter){
-		for (Vehicle vehicle : vehicles) {
-			VehicleType vehicleType = VehicleTypeImpl.Builder
-					.newInstance(vehicle.getExternalCode().toString())
-					.addCapacityDimension(WEIGHT_INDEX, vehicle.getTotalWeight().intValue())
-					.addCapacityDimension(VOLUME_INDEX, vehicle.getTotalVolume().intValue())
-					.setCostPerDistance(vehicle.getCostPerDistance())
-					.setCostPerTime(vehicle.getCostPerTime())
-					.setMaxVelocity(vehicle.getMaxVelocity())
-					.build();
-			
-			this.vehicleImpls.add(VehicleImpl.Builder
-					.newInstance(vehicle.getExternalCode().toString())
-					.setStartLocation(Location.newInstance(distributionCenter.getExternalCode().toString()))
-					.setEndLocation(Location.newInstance(distributionCenter.getExternalCode().toString()))
-					.setEarliestStart(vehicle.getEarliestStart().getTime())
-					.setLatestArrival(vehicle.getLatestEnd().getTime())
-					.setType(vehicleType)
-			.build());
-		}
-	}
-	
-	private void loadOrders(Set<Order> orders){
-		
-		for (Order order : orders) {
-			DeliveryPoint deliveryPoint = order.getDeliveryPoint();
-			
-			services.add(Service.Builder
-				.newInstance(order.getExternalCode().toString())
-				.addSizeDimension(WEIGHT_INDEX, order.getWeight())
-				.addSizeDimension(VOLUME_INDEX, order.getVolume())
-				.setServiceTime(deliveryPoint.getDeliveryDuration())
-				.setTimeWindow(TimeWindow.newInstance(0.0, order.getDeadline().getTime()))
-				.setLocation(Location.newInstance(deliveryPoint.getRoutePointExternalCode().toString()))
-				.setName(order.getName())
-				.build());
-		}
-	}
-	
-	private void loadDrivers(Set<Driver> drivers, DistributionCenter distributionCenter) {
-		for (Driver driver : drivers) {
-			DriverImpl driverImpl = DriverImpl.noDriver();
-			driverImpl.setEarliestStart(driver.getEarliestStart().getTime());
-			driverImpl.setLatestEnd(driver.getLatestEnd().getTime());
-			driverImpl.setHomeLocation(distributionCenter.getRoutePointExternalCode().toString());
-			this.drivers.add(driverImpl);
-		}
-	}
+    /**
+     * Constructs a capacitated vehicle routing problem with time windows.
+     *
+     * @param routeRequest
+     */
+    public Solver(RouteRequest routeRequest) {
+        DistributionCenter distributionCenter = routeRequest.getOrders().iterator().next().getDistributionCenter();
+        this.loadVehicles(routeRequest.getVehicles(), distributionCenter);
+        this.loadDrivers(routeRequest.getDrivers(), distributionCenter);
+        this.loadOrders(routeRequest.getOrders());
+    }
 
-	/**
-	 * Solves the current routing problem.
-	 */
-	public void solve(final RouteRequest routeRequest) {
-	}
+    private void loadVehicles(Set<Vehicle> vehicles, DistributionCenter distributionCenter) {
+        for (Vehicle vehicle : vehicles) {
+            VehicleType vehicleType =
+                    VehicleTypeImpl.Builder.newInstance(vehicle.getExternalCode().toString())
+                            .addCapacityDimension(this.WEIGHT_INDEX, vehicle.getTotalWeight().intValue())
+                            .addCapacityDimension(this.VOLUME_INDEX, vehicle.getTotalVolume().intValue())
+                            .setCostPerDistance(vehicle.getCostPerDistance()).setCostPerTime(vehicle.getCostPerTime())
+                            .setMaxVelocity(vehicle.getMaxVelocity()).build();
+
+            this.vehicleImpls.add(VehicleImpl.Builder.newInstance(vehicle.getExternalCode().toString())
+                    .setStartLocation(Location.newInstance(distributionCenter.getExternalCode().toString()))
+                    .setEndLocation(Location.newInstance(distributionCenter.getExternalCode().toString()))
+                    .setEarliestStart(vehicle.getEarliestStart().getTime())
+                    .setLatestArrival(vehicle.getLatestEnd().getTime()).setType(vehicleType).build());
+        }
+    }
+
+    private void loadOrders(Set<Order> orders) {
+
+        for (Order order : orders) {
+            DeliveryPoint deliveryPoint = order.getDeliveryPoint();
+
+            this.services.add(Service.Builder.newInstance(order.getExternalCode().toString())
+                    .addSizeDimension(this.WEIGHT_INDEX, order.getWeight())
+                    .addSizeDimension(this.VOLUME_INDEX, order.getVolume())
+                    .setServiceTime(deliveryPoint.getDeliveryDuration())
+                    .setTimeWindow(TimeWindow.newInstance(0.0, order.getDeadline().getTime()))
+                    .setLocation(Location.newInstance(deliveryPoint.getRoutePointExternalCode().toString()))
+                    .setName(order.getName()).build());
+        }
+    }
+
+    private void loadDrivers(Set<Driver> drivers, DistributionCenter distributionCenter) {
+        for (Driver driver : drivers) {
+            DriverImpl driverImpl = DriverImpl.noDriver();
+            driverImpl.setEarliestStart(driver.getEarliestStart().getTime());
+            driverImpl.setLatestEnd(driver.getLatestEnd().getTime());
+            driverImpl.setHomeLocation(distributionCenter.getRoutePointExternalCode().toString());
+            this.drivers.add(driverImpl);
+        }
+    }
+
+    /**
+     * Solves the current routing problem.
+     *
+     * @return {@link RouteResponse} calculated
+     */
+    public RouteResponse solve() {
+        return null;
+    }
 }
