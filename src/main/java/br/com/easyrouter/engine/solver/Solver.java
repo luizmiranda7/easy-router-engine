eec54a401a4ac35be86cf9b975fadaef8c0e508f
luiz.miranda <luizmiranda7@gmail.com>
2016-01-24 11:29:08 -0200

    Building solver

--- src/main/java/br/com/easyrouter/engine/solver/Solver.java
+++ src/main/java/br/com/easyrouter/engine/solver/Solver.java
@@ -1,24 +1,31 @@
 
 package br.com.easyrouter.engine.solver;
 
-
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import jsprit.core.problem.Location;
-import jsprit.core.problem.driver.DriverImpl;
-import jsprit.core.problem.job.Service;
-import jsprit.core.problem.solution.route.activity.TimeWindow;
-import jsprit.core.problem.vehicle.VehicleImpl;
-import jsprit.core.problem.vehicle.VehicleType;
-import jsprit.core.problem.vehicle.VehicleTypeImpl;
 import br.com.easyrouter.engine.api.DeliveryPoint;
+import br.com.easyrouter.engine.api.DirectionLeg;
 import br.com.easyrouter.engine.api.DistributionCenter;
-import br.com.easyrouter.engine.api.Driver;
 import br.com.easyrouter.engine.api.Order;
 import br.com.easyrouter.engine.api.RouteRequest;
 import br.com.easyrouter.engine.api.RouteResponse;
 import br.com.easyrouter.engine.api.Vehicle;
+import jsprit.core.algorithm.VehicleRoutingAlgorithm;
+import jsprit.core.algorithm.io.VehicleRoutingAlgorithms;
+import jsprit.core.problem.Location;
+import jsprit.core.problem.VehicleRoutingProblem;
+import jsprit.core.problem.VehicleRoutingProblem.FleetSize;
+import jsprit.core.problem.driver.DriverImpl;
+import jsprit.core.problem.job.Shipment;
+import jsprit.core.problem.solution.VehicleRoutingProblemSolution;
+import jsprit.core.problem.solution.route.activity.TimeWindow;
+import jsprit.core.problem.vehicle.VehicleImpl;
+import jsprit.core.problem.vehicle.VehicleType;
+import jsprit.core.problem.vehicle.VehicleTypeImpl;
+import jsprit.core.util.Solutions;
+import jsprit.core.util.VehicleRoutingTransportCostsMatrix;
 
 
 public class Solver {
@@ -27,8 +34,8 @@ public class Solver {
     private final int VOLUME_INDEX = 1;
 
     private Set<VehicleImpl> vehicleImpls = new HashSet<VehicleImpl>();
-    private Set<Service> services = new HashSet<Service>();
-    private Set<DriverImpl> drivers = new HashSet<DriverImpl>();
+    private Set<Shipment> shipments = new HashSet<Shipment>();
+    private VehicleRoutingTransportCostsMatrix costMatrix = null;
 
     /**
      * Constructs a capacitated vehicle routing problem with time windows.
@@ -37,12 +44,34 @@ public class Solver {
      */
     public Solver(RouteRequest routeRequest) {
         DistributionCenter distributionCenter = routeRequest.getOrders().iterator().next().getDistributionCenter();
-        this.loadVehicles(routeRequest.getVehicles(), distributionCenter);
-        this.loadDrivers(routeRequest.getDrivers(), distributionCenter);
-        this.loadOrders(routeRequest.getOrders());
+        this.vehicleImpls = this.loadVehicles(routeRequest.getVehicles(), distributionCenter);
+        this.shipments = this.loadShipments(routeRequest.getOrders(), distributionCenter);
+        this.costMatrix = this.loadCostMatrix(routeRequest.getDirectionLegs());
+        
     }
 
-    private void loadVehicles(Set<Vehicle> vehicles, DistributionCenter distributionCenter) {
+    private VehicleRoutingTransportCostsMatrix loadCostMatrix(Set<DirectionLeg> directionLegs) {
+    	VehicleRoutingTransportCostsMatrix.Builder costMatrixBuilder = VehicleRoutingTransportCostsMatrix.Builder.newInstance(true);
+    	
+    	for (DirectionLeg directionLeg : directionLegs) {
+    		String initialPointId = directionLeg.getInitialPoint().toString();
+    		String finalPointId = directionLeg.getFinalPoint().toString();
+    		costMatrixBuilder.addTransportDistance(
+					initialPointId,
+					finalPointId, 
+					directionLeg.getDistance());
+			
+    		costMatrixBuilder.addTransportTime(
+					initialPointId,
+					finalPointId,
+					directionLeg.getDuration());
+		}
+    	return costMatrixBuilder.build();
+	}
+
+	private Set<VehicleImpl> loadVehicles(Set<Vehicle> vehicles, DistributionCenter distributionCenter) {
+		HashSet<VehicleImpl> result = new HashSet<VehicleImpl>();
+		
         for (Vehicle vehicle : vehicles) {
             VehicleType vehicleType =
                     VehicleTypeImpl.Builder.newInstance(vehicle.getExternalCode().toString())
@@ -51,37 +80,32 @@ public class Solver {
                             .setCostPerDistance(vehicle.getCostPerDistance()).setCostPerTime(vehicle.getCostPerTime())
                             .setMaxVelocity(vehicle.getMaxVelocity()).build();
 
-            this.vehicleImpls.add(VehicleImpl.Builder.newInstance(vehicle.getExternalCode().toString())
+            result.add(VehicleImpl.Builder.newInstance(vehicle.getExternalCode().toString())
                     .setStartLocation(Location.newInstance(distributionCenter.getExternalCode().toString()))
                     .setEndLocation(Location.newInstance(distributionCenter.getExternalCode().toString()))
                     .setEarliestStart(vehicle.getEarliestStart().getTime())
                     .setLatestArrival(vehicle.getLatestEnd().getTime()).setType(vehicleType).build());
         }
+        return result;
     }
 
-    private void loadOrders(Set<Order> orders) {
-
+    private Set<Shipment> loadShipments(Set<Order> orders, DistributionCenter distributionCenter) {
+    	HashSet<Shipment> result = new HashSet<Shipment>();
         for (Order order : orders) {
             DeliveryPoint deliveryPoint = order.getDeliveryPoint();
 
-            this.services.add(Service.Builder.newInstance(order.getExternalCode().toString())
+            result.add(Shipment.Builder.newInstance(order.getExternalCode().toString())
                     .addSizeDimension(this.WEIGHT_INDEX, order.getWeight())
                     .addSizeDimension(this.VOLUME_INDEX, order.getVolume())
-                    .setServiceTime(deliveryPoint.getDeliveryDuration())
-                    .setTimeWindow(TimeWindow.newInstance(0.0, order.getDeadline().getTime()))
-                    .setLocation(Location.newInstance(deliveryPoint.getRoutePointExternalCode().toString()))
+                    .setDeliveryServiceTime(deliveryPoint.getDeliveryDuration())
+                    .setDeliveryTimeWindow(TimeWindow.newInstance(0.0, order.getDeadline().getTime()))
+                    .setPickupServiceTime(distributionCenter.getPrepareDuration())
+                    .setDeliveryLocation(Location.newInstance(deliveryPoint.getRoutePointExternalCode().toString()))
+                    .setPickupLocation(Location.newInstance(deliveryPoint.getRoutePointExternalCode().toString()))
                     .setName(order.getName()).build());
-        }
-    }
-
-    private void loadDrivers(Set<Driver> drivers, DistributionCenter distributionCenter) {
-        for (Driver driver : drivers) {
-            DriverImpl driverImpl = DriverImpl.noDriver();
-            driverImpl.setEarliestStart(driver.getEarliestStart().getTime());
-            driverImpl.setLatestEnd(driver.getLatestEnd().getTime());
-            driverImpl.setHomeLocation(distributionCenter.getRoutePointExternalCode().toString());
-            this.drivers.add(driverImpl);
-        }
+        };
+        
+        return result;
     }
 
     /**
@@ -90,6 +114,19 @@ public class Solver {
      * @return {@link RouteResponse} calculated
      */
     public RouteResponse solve() {
-        return null;
+
+		VehicleRoutingProblem problem = VehicleRoutingProblem.Builder
+				.newInstance()
+				.setRoutingCost(this.costMatrix)
+				.setFleetSize(FleetSize.FINITE)
+				.addAllVehicles(this.vehicleImpls)
+				.addAllJobs(this.shipments)
+				.build();
+		
+		VehicleRoutingAlgorithm algorithm = VehicleRoutingAlgorithms.readAndCreateAlgorithm(problem, "input/algorithmConfig.xml");
+		Collection<VehicleRoutingProblemSolution> solutions = algorithm.searchSolutions();
+		VehicleRoutingProblemSolution bestSolution = Solutions.bestOf(solutions);
+		
+		return null;
     }
 }
